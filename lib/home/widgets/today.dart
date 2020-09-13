b8e13460014bfdd2cacc0089d9eab281c1909716
Paloma Piot <piot.paloma@gmail.com>
2020-09-13 16:22:34 +0200

    Symptoms repository (partially)

--- lib/home/widgets/today.dart
+++ lib/home/widgets/today.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:radarescolas/symptoms/symptoms.dart';
+
+import 'package:symptoms_repository/symptoms_repository.dart';
 
 class Today extends StatelessWidget {
   Today({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Today"));
+    return BlocProvider<SymptomsCubit>(
+        create: (_) => SymptomsCubit(
+          context.repository<SymptomsRepository>(),
+        ),
+        child: SymptomsForm()
+    );
   }
 }
